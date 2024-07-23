d84174c112632ef5f3084aee12efa448c87e3fd4
Yana Stepanova <yanna.stepanova@gmail.com>
2024-07-23 19:14:46 +0300

    intro

---
+++ src/main/java/stepanova/yana/AccommodationBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package stepanova.yana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccommodationBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccommodationBookingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/stepanova/yana/AccommodationBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package stepanova.yana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccommodationBookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
