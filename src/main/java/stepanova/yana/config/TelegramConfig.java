cbc037e21d8ba8f7bcf251952d57bcf0637cebe2
Yana Stepanova <yanna.stepanova@gmail.com>
2024-11-03 03:57:11 +0200

    telegram_bot sent message about created/updated accommodation in telegram-group!!!

---
+++ src/main/java/stepanova/yana/config/TelegramConfig.java
@@ -0,0 +1,23 @@
+package stepanova.yana.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import stepanova.yana.telegram.TelegramNotificationService;
+
+@RequiredArgsConstructor
+@Configuration
+public class TelegramConfig {
+    private final TelegramNotificationService telegram;
+
+    @Bean
+    public TelegramBotsApi telegramBotsApi(TelegramNotificationService telegram)
+            throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(telegram);
+        return botsApi;
+    }
+}
