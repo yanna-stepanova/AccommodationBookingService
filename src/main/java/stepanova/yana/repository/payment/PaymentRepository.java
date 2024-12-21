4c5833cc8448751cbbfb435aa1f4efb0bda3d134
Yana Stepanova <yanna.stepanova@gmail.com>
2024-12-21 02:35:19 +0200

    changed 'left join' -> 'inner join'

--- src/main/java/stepanova/yana/repository/payment/PaymentRepository.java
+++ src/main/java/stepanova/yana/repository/payment/PaymentRepository.java
@@ -22,14 +22,14 @@ public interface PaymentRepository extends JpaRepository<Payment, Long> {
     Optional<Payment> findByBookingId(Long id);
 
     @Query(value = "SELECT p.id, p.status, p.date_time_created, p.booking_id, p.amount_to_pay, "
-            + "p.session_url, p.session_id, p.is_deleted FROM payments p LEFT JOIN bookings b "
+            + "p.session_url, p.session_id, p.is_deleted FROM payments p INNER JOIN bookings b "
             + "ON p.booking_id = b.id WHERE p.session_id = :sessionId AND b.user_id = :userId ",
             nativeQuery = true)
     Optional<Payment> findBySessionIdAndUserId(@Param("sessionId") String sessionId,
                                                @Param("userId") Long userId);
 
     @Query(value = "SELECT p.id, p.status, p.date_time_created, p.booking_id, p.amount_to_pay, "
-            + "p.session_url, p.session_id, p.is_deleted FROM payments p LEFT JOIN bookings b "
+            + "p.session_url, p.session_id, p.is_deleted FROM payments p INNER JOIN bookings b "
             + "ON p.booking_id = b.id WHERE b.user_id = :userId",
             nativeQuery = true)
     List<Payment> findAllByUserId(@Param("userId") Long userId);
