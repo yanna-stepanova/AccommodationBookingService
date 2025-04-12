62fe8d077377abe1d31d359523a21ec8b5936f4b
Yana Stepanova <yanna.stepanova@gmail.com>
2025-04-12 10:52:32 +0300

    changed relation

--- src/main/java/stepanova/yana/model/Accommodation.java
+++ src/main/java/stepanova/yana/model/Accommodation.java
@@ -1,6 +1,5 @@
 package stepanova.yana.model;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -12,7 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.util.HashSet;
@@ -39,8 +38,7 @@ public class Accommodation {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Type type;
-    @OneToOne(fetch = FetchType.LAZY, optional = false,
-            cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "location_id", nullable = false)
     private Location location;
     @Column(nullable = false)
--- src/main/java/stepanova/yana/model/User.java
+++ src/main/java/stepanova/yana/model/User.java
@@ -10,7 +10,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
